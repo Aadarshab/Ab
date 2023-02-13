d2deb2852e0741b028a55d90fafd50d4fdd97c5d
Aadarshab <72687881+Aadarshab@users.noreply.github.com>
2023-02-14 02:06:12 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Aadarshab/Ab.git /Ab
-cd /Ab
+git clone https://github.com/Aadarshab/Ab /ab
+cd /ab
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
