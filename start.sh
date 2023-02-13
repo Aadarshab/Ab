0763b2f4d4d774ee8508f5d00da90e6c2828f67b
Aadarshab <72687881+Aadarshab@users.noreply.github.com>
2023-02-14 01:24:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Aadarshab/Ab.git /ab
-cd /ab
+git clone https://github.com/Aadarshab/Ab.git /Ab
+cd /Ab
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
